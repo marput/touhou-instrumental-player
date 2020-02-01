96b3b93ee6bc25049bf5ad50685fb1f7172cb35f
marput <frayedultrasonicaligator@disroot.org>
2020-02-01 21:05:26 +0100

    Added optional command-line argument which points to a file that lists strings with it's respective scores. If sum of all hits is below 0, the file is skipped.

--- touhou-instrumental-player.py
+++ touhou-instrumental-player.py
@@ -5,10 +5,12 @@ import re
 import time
 import subprocess
 import signal
+import sys
 from bs4 import BeautifulSoup
 
 instrumental = "https://invidio.us/search?q=touhou%20instrumental"
 bgm = "https://invidio.us/search?q=touhou+bgm"
+classical = "https://invidio.us/search?q=touhou+classical"
 
 def changePage(url, page):
     url = re.sub(r'&page=\d*', '', url)
@@ -61,7 +63,7 @@ def getListOfHrefs(session, url, page):
     for element in temp:
         a.append((element.find('a')['href'], element.find('p', class_=False).text))
     firstBannedExpression = r"playlist"
-    secondBannedExpression = r"vocal|subbed|subs|sub|cover"
+    secondBannedExpression = r"vocal|subbed|subs|sub|cover|lyrics|Touhou Sky Arena Matsuri Climax Music Extended|Smooth McGroove"
     listOfHrefs = []
     for element in a:
        firstMatch = re.search(firstBannedExpression, element[0])
@@ -90,30 +92,79 @@ def getChoice(prompt):
             choice = input(prompt)
         except:
             return choice
-        if choice == "1" or choice == "2":
+        if choice == "1" or choice == "2" or choice == "3" or choice == "Q":
             signal.alarm(0)
             return choice
 
-links = [instrumental, bgm]
-mainTheme = random.randint(0,1) 
+links = [instrumental, bgm, classical]
+mainTheme = random.randint(0,2) 
+
+def score(title):
+    score = 0
+    f = open(sys.argv[1], "r")
+    for line in f:
+        line_divided = line.split(',', maxsplit=1)
+        line_text = line_divided[0]
+        line_score = int(line_divided[1])
+        match = re.search(line_text, title, re.IGNORECASE)
+        if match:
+            score+=line_score
+    f.close()
+    return score
+
+def getFilterInput(message, title):
+    while True:
+        user_input = input(message)
+        match = re.search(r"^.*\,.*\d+$", user_input)
+        if not match and user_input != "A":
+            print("Wrongly formatted input, try again.\n")
+            continue
+        if user_input == "A":
+            user_input = title
+            user_score = input("Enter the score: ")
+            try:
+                int(user_score)
+            except ValueError:
+                print("Wrongly formatted score, try again.\n")
+                continue
+            return user_input + ',' + str(user_score) + '\n'
+        return user_input + '\n'
+
+def addFilter(title):
+    f = open(sys.argv[1], "a")
+    message = "Enter the filter, separated by a , from the score. Negative score needs - prefix; eg. niggy, -50. Escape commas with '\\'. To add the entire title to filters, enter 'A'\n"
+    f.write(getFilterInput(message, title))
+    
 
 session = requests.Session()
 session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'})
 
+print("Getting highest page...")
 upperBound = getUpperBound(session, instrumental, 1)
 highestPage = getHighestPage(session, links[mainTheme], upperBound/4-1, upperBound)
 randomPage = random.randint(1,highestPage)
 listOfVideos = getListOfVideos(getListOfHrefs(session, links[mainTheme], randomPage))
 while True:
     for element in listOfVideos:
-        print(element[1])
-        print(element[0])
+        if len(sys.argv) > 1:
+            points = score(element[1])
+            if points <= 0:
+                continue
+        title = element[1]
+        address = element[0]
+        print(title)
+        print(address)
+        print("Touhou-ness score: " + str(points))
         subprocess.call(["mpv", "--no-video", "--af=scaletempo=speed=both", "--audio-pitch-correction=no", "--ytdl-format=bestaudio", element[0]])
-        choice = getChoice("Press 1 to download the file, press 2 to go to the next.\n")
+        choice = getChoice("Press 1 to download the file, press 2 to go to the next, press 3 to add a new filter, press Q to quit.\n")
         if choice == "1":
             subprocess.call(["youtube-dl", "-x", "-o", "'%(title)s.%(ext)s'", "--audio-quality", "0", "--audio-format", "flac", element[0]])
         elif choice == "2":
             pass
+        elif choice == "3":
+            addFilter(title)
+        elif choice == "Q":
+            sys.exit()
         else:
             pass
     randomPage = random.randint(1, int(highestPage))
